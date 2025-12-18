4853a9beec2f8aea63a3f751b35e38b1c3a43f72
Jung wooseok <jungwooseok@wooseok-MacBook-Pro.local>
2025-12-18 11:30:59 +0900

    Solve Euler problem 001

---
+++ p001.py
@@ -0,0 +1,10 @@
+def solve():
+    total = 0
+    for i in range(1000):
+        if i % 3 == 0 or i % 5 == 0:
+            total += i
+    return total
+
+
+if __name__ == "__main__":
+    print(solve())
